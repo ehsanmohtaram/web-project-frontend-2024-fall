e2404e8b96fd84567b87510742a02f1940b2eda9
SHSMJ200 <shjasbi200@gmail.com>
2024-12-18 19:47:29 +0330

    connection between front and back

--- app/src/view/D_question_management.js
+++ app/src/view/D_question_management.js
@@ -1,48 +1,69 @@
+import React,{useEffect,useState} from 'react';
+
 function D_question_management() {
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(()=>{
+        fetch("http://localhost:3001/questions")
+            .then(res=>res.json())
+            .then(data=>setQuestions(data))
+            .catch(error => {
+                console.log(error);
+            })
+    },[])
+
     return (
         <article class="questions">
-        <h1>Questions</h1>
-        <section class="question">
-            <h4>How many letters are there in "hello"?</h4>
+            <h1>Questions</h1>
             <section>
-                <input id="q_1_1" name="question_1" type="radio"/>
-                <label for="q_1_1">2</label>
+                {questions.map((question) => (
+                    <div>
+                        <h4>{question.ask}</h4>
+                        <p>{question.seed}</p>
+                    </div>
+                ))}
             </section>
-            <section>
-                <input id="q_1_2" name="question_1" type="radio"/>
-                <label for="q_1_2">3</label>
+            {/* <section class="question">
+                <h4>How many letters are there in "hello"?</h4>
+                <section>
+                    <input id="q_1_1" name="question_1" type="radio"/>
+                    <label for="q_1_1">2</label>
+                </section>
+                <section>
+                    <input id="q_1_2" name="question_1" type="radio"/>
+                    <label for="q_1_2">3</label>
+                </section>
+                <section>
+                    <input id="q_1_3" name="question_1" type="radio"/>
+                    <label for="q_1_3">4</label>
+                </section>
+                <section>
+                    <input id="q_1_4" name="question_1" type="radio"/>
+                    <label for="q_1_4">5</label>
+                </section>
+                <button class="submit">Submit Answer</button>
             </section>
-            <section>
-                <input id="q_1_3" name="question_1" type="radio"/>
-                <label for="q_1_3">4</label>
-            </section>
-            <section>
-                <input id="q_1_4" name="question_1" type="radio"/>
-                <label for="q_1_4">5</label>
-            </section>
-            <button class="submit">Submit Answer</button>
-        </section>
-        <section class="question">
-            <h4>Where is the capital of Iran?</h4>
-            <section>
-                <input id="q_2_1" name="question_2" type="radio"/>
-                <label for="q_2_1">Tehran</label>
-            </section>
-            <section>
-                <input id="q_2_2" name="question_2" type="radio"/>
-                <label for="q_2_2">Mashhad</label>
-            </section>
-            <section>
-                <input id="q_2_3" name="question_2" type="radio"/>
-                <label for="q_2_3">Esfehan</label>
-            </section>
-            <section>
-                <input id="q_2_4" name="question_2" type="radio"/>
-                <label for="q_2_4">Yazd</label>
-            </section>
-            <button class="submit">Submit Answer</button>
-        </section>
-    </article>
+            <section class="question">
+                <h4>Where is the capital of Iran?</h4>
+                <section>
+                    <input id="q_2_1" name="question_2" type="radio"/>
+                    <label for="q_2_1">Tehran</label>
+                </section>
+                <section>
+                    <input id="q_2_2" name="question_2" type="radio"/>
+                    <label for="q_2_2">Mashhad</label>
+                </section>
+                <section>
+                    <input id="q_2_3" name="question_2" type="radio"/>
+                    <label for="q_2_3">Esfehan</label>
+                </section>
+                <section>
+                    <input id="q_2_4" name="question_2" type="radio"/>
+                    <label for="q_2_4">Yazd</label>
+                </section>
+                <button class="submit">Submit Answer</button>
+            </section> */}
+        </article>
     );
   }
   
