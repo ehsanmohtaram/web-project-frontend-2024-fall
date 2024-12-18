945c360278206d2cf0caa107728853eab7fb940d
SHSMJ200 <shjasbi200@gmail.com>
2024-12-18 18:03:11 +0330

    complete the navbar

--- app/src/Layout/Navbar.js
+++ app/src/Layout/Navbar.js
@@ -7,31 +7,61 @@ function Navbar() {
     const location = useLocation();
     return (
         <nav>
-            <ul>
-                {location.pathname === '/' && (
-                    <>
-                        <li class="chosen_a"><Link to="/">Home</Link></li>
-                        <li><Link to="/designer">Designer</Link></li>
-                        <li><Link to="/player">Player</Link></li>
-                    </>
-                )}
-                {location.pathname === '/designer' && (
-                    <>
-                        <li><Link to="/">Home</Link></li>
-                        <li class="chosen_a"><Link to="/designer">Designer</Link></li>
-                        <li><Link to="/designer/questions_management">Question Management</Link></li>
-                        <li><Link to="/designer/seed_management">Seed Management</Link></li>
-                    </>
-                )}
-                {location.pathname === '/player' && (
-                    <>
-                        <li><Link to="/">Home</Link></li>
-                        <li class="chosen_a"><Link to="/player">Player</Link></li>
-                        <li><Link to="/player/questions_management">Question Management</Link></li>
-                        <li><Link to="/player/scores">Scores</Link></li>
-                    </>
-                )}
-            </ul>
+            {location.pathname === '/' && (
+                <>
+                    <Link class="chosen_a" to="/">Home</Link>
+                    <Link to="/designer">Designer</Link>
+                    <Link to="/player">Player</Link>
+                </>
+            )}
+            {location.pathname === '/designer' && (
+                <>
+                <Link to="/">Home</Link>
+                <Link class="chosen_a" to="/designer">Designer</Link>
+                <Link to="/designer/question_management">Question Management Page</Link>
+                <Link to="/designer/seed_management">Seed Management Page</Link>
+            </>
+            )}
+            {location.pathname === '/designer/question_management' && (
+                <>
+                <Link to="/">Home</Link>
+                <Link to="/designer">Designer</Link>
+                <Link class="chosen_a" to="/designer/question_management">Question Management Page</Link>
+                <Link to="/designer/seed_management">Seed Management Page</Link>
+            </>
+            )}
+            {location.pathname === '/designer/seed_management' && (
+                <>
+                <Link to="/">Home</Link>
+                <Link to="/designer">Designer</Link>
+                <Link to="/designer/question_management">Question Management Page</Link>
+                <Link class="chosen_a" to="/designer/seed_management">Seed Management Page</Link>
+            </>
+            )}
+            {location.pathname === '/player' && (
+                <>
+                <Link to="/">Home</Link>
+                <Link class="chosen_a" to="/player">Player</Link>
+                <Link to="/player/scores">Scores Page</Link>
+                <Link to="/player/question_management">Question Management Page</Link>
+            </>
+            )}
+            {location.pathname === '/player/scores' && (
+                <>
+                <Link to="/">Home</Link>
+                <Link to="/player">Player</Link>
+                <Link class="chosen_a" to="/player/scores">Scores Page</Link>
+                <Link to="/player/question_management">Question Management Page</Link>
+            </>
+            )}
+            {location.pathname === '/player/question_management' && (
+                <>
+                <Link to="/">Home</Link>
+                <Link to="/player">Player</Link>
+                <Link to="/player/scores">Scores Page</Link>
+                <Link class="chosen_a" to="/player/question_management">Question Management Page</Link>
+            </>
+            )}
             {/* <button id="colorButton" class="dayNightButton">
                 Day Time
             </button> */}
