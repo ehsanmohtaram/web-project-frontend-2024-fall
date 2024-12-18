3796b01b488eab675162a2f734b825388c5f6379
Ehsan <ehsanmohtaram2004@gmail.com>
2024-12-19 00:11:40 +0330

    loginBox

--- app/src/Components/LoginBox.js
+++ app/src/Components/LoginBox.js
@@ -1,18 +1,73 @@
 import '../App.css';
+import React, { useState } from "react";
+
 
 function LoginBox() {
+    const [username, setUsername] = useState("")
+    const [password, serPassword] = useState("")
+    let respons = ""
+
+    const handleInputChangeU = (event) => {
+        setUsername(event.target.value);
+    };
+    const handleInputChangeP = (event) => {
+        serPassword(event.target.value)
+    };
+
+    const singIn = () => {
+        const newUser = {
+            "username" : username,
+            "password" : password
+        }
+        fetch("http://localhost:3001/addUser", {
+            "method" : "POST",
+            body : JSON.stringify(newUser),
+            headers : {
+                "content-type" : "application/json"
+            }
+        })
+            .then(res=>res.json())
+            .then(data=>respons = data)
+            .catch(error => {
+                console.log(error);
+            })
+        alert(respons)
+    }
+
+    const singUp = () => {
+        const newUser = {
+            "username" : username,
+            "password" : password
+        }
+        fetch("http://localhost:3001/user", {
+            "method" : "POST",
+            body : JSON.stringify(newUser),
+            headers : {
+                "content-type" : "application/json"
+            }
+        })
+            .then(res=>res.json())
+            .then(data=>respons = data)
+            .catch(error => {
+                console.log(error);
+            })
+        alert(respons)
+    }
+
+
+
     return (
         <section class="login_box">
             <div>
                 <label className="login_label" htmlFor="username">Username </label>
-                <input type="text" name="username" placeholder="username" defaultValue="" />
+                <input type="text" placeholder="username" value={username} onChange={handleInputChangeU} defaultValue="" />
             </div>
             <div>
-                <label class="login_label" for="password">Password </label>
-                <input type="text" name="password" defaultValue="" placeholder="password" />
+                <label className="login_label" for="password">Password </label>
+                <input type="text" value={password} onChange={handleInputChangeP} defaultValue="" placeholder="password" />
             </div>
-            <button class="add_button">Sign In</button>
-            <button class="add_button">Sign Up</button>
+            <button className="add_button" onClick={singIn}>Sign In</button>
+            <button class="add_button" onClick={singUp}>Sign Up</button>
         </section>
     );
 }
